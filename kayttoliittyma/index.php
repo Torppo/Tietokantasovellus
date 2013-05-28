893d9716774dd9d979361689cc04976b12f1300b
Pii A J Sainio <pisapisa@svm-19.cs.helsinki.fi>
2013-05-28 13:22:31 +0300

    lisays

---
+++ kayttoliittyma/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+ <head>
+  <title>Etusivu</title>
+ </head>
+ <body>
+ 
+	<h1>Kaupunkitietokanta</h1>
+
+	<iframe src="http://pisapisa.users.cs.helsinki.fi/yleisetLinkit.php" width="800" height="60"></iframe>
+
+	<h3>Yleishaku</h3>
+
+	<h2>h2</h2>
+	<p>Esittely</p>
+
+	<iframe src="http://pisapisa.users.cs.helsinki.fi/kaikkiLinkit.php" width="800" height="300"></iframe>
+
+ </body>
+</html>
